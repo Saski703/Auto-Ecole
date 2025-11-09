db7324f9347013f1a61b3345ac422530e3856a39
Saski703 <hichriomar036@gmail.com>
2025-11-09 21:32:36 +0100

    Add attribut vihecule dans Conduit.java

--- src/Models/Conduit.java
+++ src/Models/Conduit.java
@@ -4,8 +4,9 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 
 public class Conduit extends Seance{
-
-    Conduit(int num, LocalDate date, LocalTime heure, Moniteur moniteur, int prix) {
+    private Vehicule vehicule;
+    Conduit(int num, LocalDate date, LocalTime heure, Moniteur moniteur, int prix, Vehicule vehicule) {
         super(num, date, heure, moniteur, prix);
+        this.vehicule = vehicule;
     }
 }
