0afefb6680e53d50ec0dc1329a1cd8c4d8760fcc
mohamedaminemsalhi-droid <mohamedamine.msalhi@fsb.ucar.tn>
2025-11-09 17:02:31 +0100

    aaa

--- src/main.java
+++ src/main.java
@@ -1,5 +1,9 @@
 public class main {
     public static void main(String[] args) {
+        System.out.println("m3ch wa9t");
+        System.out.println("yilzimna nizrbou");
+        System.out.println("aaaa");
+
 
 
 
