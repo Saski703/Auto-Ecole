61a53b5e41e7b07c9f69925f985606594164f430
Saski703 <hichriomar036@gmail.com>
2025-11-10 20:29:21 +0100

    test

--- src/Models/Conduit.java
+++ src/Models/Conduit.java
@@ -4,6 +4,7 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 
 public class Conduit extends Seance{
+
     private Vehicule vehicule;
     Conduit(int num, LocalDate date, LocalTime heure, Moniteur moniteur, int prix, Vehicule vehicule) {
         super(num, date, heure, moniteur, prix);
